cd1df82f3b2ee2aef885087419bb74af892e70b8
Yuvraj <ys2029472@gmail.com>
2024-01-12 12:50:32 +0530

    uPDATE COMMENT

--- cypress/e2e/practice.3cy.js
+++
@@ -1,9 +0,0 @@
-describe('',()=>{
-
-    it('', () => {
-        cy.visit("https://support.google.com/mail/answer/56256?hl=en")
-        cy.get("a.action-button.btn.btn-raised:contains('Create an account')").should('be.visible').invoke('removeAttr','target').click()
-        /// java students are idiot dont no how to make branch in git
-    
-    });
-})
